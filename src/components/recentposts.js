c7bd344da55be2bb5d563d71b2e119aaa87117df
Jrogerson74 <jrsquish@gmail.com>
2020-01-02 10:01:18 -0700

    added post topics to post component

--- src/components/recentposts.js
+++ src/components/recentposts.js
@@ -4,25 +4,24 @@ import { connect } from 'react-redux';
 
 import * as actions from '../actions';
 
-class RecentPost extends Component {
+import Post from './post';
+
+class RecentPosts extends Component {
 
     componentDidMount() {
         this.props.fetchRecentPosts();
     }
 
     renderPosts = function() {
-        const posts = this.props.recentPosts.map((post, index) => {           
+        const posts = this.props.recentPosts.map((post, index) => {
             if(index < 3) {
                 return (
-                    <li key={index}>
-                        {post.title}
-                    </li>
-                )
+                    <Post {...post} key={index}/>
+                )   
             }
         })
         return posts
     }
-
     render() {
         return (
             <div className="recent-posts">
@@ -32,16 +31,14 @@ class RecentPost extends Component {
                         {this.renderPosts()}
                     </ul>
                 </div>
-
             </div>
         )
     }
 }
-
 function mapStateToProps(state) {
     return {
         recentPosts: state.posts.recentPosts
     }
 }
 
-export default connect(mapStateToProps, actions)(RecentPost); 
+export default connect(mapStateToProps, actions)(RecentPosts); 
